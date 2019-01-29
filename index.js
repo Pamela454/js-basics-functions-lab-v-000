c00d4cf4ea251d68ade08e3dc435cfdc9979b7c8
Pamela Frieze <pfrieze@gmail.com>
2019-01-29 19:47:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function distanceFromHqInBlocks (blockDistance) {
   if (blockDistance > 42) {
-    return BlockDistance - 42;
+    return blockDistance - 42;
   } else {
   return 42 - BlockDistance;
   }
