a5de5509c4e0980c827b080195e344e526baf864
Pamela Frieze <pfrieze@gmail.com>
2019-01-29 19:56:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 
-function distanceFromHqInBlocks (blockNumber) {
+distanceFromHqInBlocks (blockNumber) {
   if (blockNumber > 42) {
     return blockNumber - 42;
   } else {
